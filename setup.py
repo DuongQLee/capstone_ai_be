f2c616d3a4f4c95dfcc7c77a4015d19df73884d6
Duong Le <duongqlee@gmail.com>
2024-09-21 21:46:07 +0700

    change path

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import gdown
 gdown.download(
-    f"https://drive.google.com/uc?export=download&confirm=pbef&id={"14-IRbqyojhmiNoY7dH6aWGwDad6Z56af"}",
+    f"https://drive.google.com/uc?export=download&confirm=pbef&id=14ab3wYzcLQTJyeWLRZx7r0U_EWNl5sIL",
     "models/segmentation_model.pt"
 )
